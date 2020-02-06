a5312818f5dd001dbba71220d8f60d2150441898
RichardW <richard.webb@affiliate.oliverwyman.com>
2020-02-06 23:17:33 +0000

    add basic test that an SVG element exists

--- src/components/nav-wheel.test.js
+++ src/components/nav-wheel.test.js
@@ -1,9 +1,64 @@
 import { shallowMount } from "@vue/test-utils";
 import NavWheel from "./nav-wheel.vue";
+import baseConfig from "../nav-wheel-config";
+
+const testConfig = {
+  ...baseConfig,
+  routes: [
+    {
+      name: "1",
+      path: "/styles-2-1",
+      component: {
+        template: "<div></div>"
+      },
+      meta: { navWheel: { style: { stroke: "red" }, isDisabled: true } }
+    },
+    {
+      name: "2",
+      path: "/styles-2/2",
+      component: {
+        template: "<div></div>"
+      },
+      meta: {
+        navWheel: {
+          transitionName: "fade",
+          style: { stroke: "yellow", fill: "purple" }
+        }
+      },
+      children: [
+        {
+          name: "2-1",
+          path: "/styles-2/2/1",
+          component: {
+            template: "<div></div>"
+          },
+          meta: {
+            navWheel: { style: { stroke: "yellow" }, isHidden: true }
+          }
+        },
+        {
+          name: "2-2",
+          path: "/styles-2/2/2",
+          component: {
+            template: "<div></div>"
+          }
+        }
+      ]
+    }
+  ]
+};
 
 describe("nav-wheel.vue", () => {
   // Basic functionality
-  xit("renders an SVG element", () => {});
+  it("renders an SVG element", () => {
+    const wrapper = shallowMount(NavWheel, {
+      propsData: {
+        config: testConfig,
+        size: 600
+      }
+    });
+    expect(wrapper.find("svg").exists()).toBe(true);
+  });
   xit("scales the SVG according to DOM mousewheel", () => {});
   xit("scales only the SVG, not the parent container", () => {});
   xit("pans according to DOM mousemove", () => {});
